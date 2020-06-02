e1214fa72bf4ed01d9b1b5ac992556d3a9444b15
Simon <bummy93@gmx.de>
2020-06-02 09:20:22 +0000

    MVP: still super messy code base but OAuth + Webhook handshake seem to work

--- config/custom_components/ha_strava/__init__.py
+++ config/custom_components/ha_strava/__init__.py
@@ -1,13 +1,19 @@
 """The Strava Home Assistant integration."""
 import asyncio
+import logging
+import json
+import voluptuous as vol
+from aiohttp.web import json_response, Response
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from aiohttp import MultipartWriter
+
 from homeassistant.helpers.network import get_url
 
 AUTH_CALLBACK_PATH = "/auth/external/callback"
 
-import voluptuous as vol
-
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_CLIENT_ID, CONF_CLIENT_SECRET
+from homeassistant.config_entries import ConfigEntry, SOURCE_IMPORT
+from homeassistant.const import CONF_CLIENT_ID, CONF_CLIENT_SECRET, HTTP_OK
+from homeassistant.components.http.view import HomeAssistantView
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import (
     aiohttp_client,
@@ -15,7 +21,7 @@ from homeassistant.helpers import (
     config_validation as cv,
 )
 
-from . import config_flow
+from .config_flow import OAuth2FlowHandler
 from .const import DOMAIN, OAUTH2_AUTHORIZE, OAUTH2_TOKEN
 
 CONFIG_SCHEMA = vol.Schema(
@@ -30,6 +36,8 @@ CONFIG_SCHEMA = vol.Schema(
     extra=vol.ALLOW_EXTRA,
 )
 
+_LOGGER = logging.getLogger(__name__)
+
 # TODO List the platforms that you want to support.
 # For your initial PR, limit it to 1 platform.
 # PLATFORMS = ["sensor"]
@@ -37,13 +45,62 @@ PLATFORMS = []
 
 
 async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the Strava Home Assistant component."""
+    """
+    Set up the Strava Home Assistant component.
+    This function is only called once upon the installation of the integration
+    """
+
+    _LOGGER.debug("called async setup")
+
     hass.data[DOMAIN] = {}
 
     if DOMAIN not in config:
         return True
 
-    config_flow.OAuth2FlowHandler.async_register_implementation(
+    subscribe_view = StravaSubscribeView()
+    hass.http.register_view(subscribe_view)
+
+    """
+    if post_response.status == 400:
+        post_response_content = await post_response.text()
+
+        if "exists" in json.loads(post_response_content)["errors"][0]["code"]:
+            _LOGGER.debug("get all the details")
+            params = {
+                "client_id": config[DOMAIN][CONF_CLIENT_ID],
+                "client_secret": config[DOMAIN][CONF_CLIENT_SECRET],
+            }
+
+            get_response = await websession.get(url=request_url, params=params)
+
+            get_response_content = json.loads(await get_response.text())[0]
+            webhook_id = get_response_content["id"]
+            callback_url = get_response_content["callback_url"]
+
+            if callback_url != CALLBACK_URL:
+                _LOGGER.info("Updating callback URL for strava webhook")
+
+                delete_response = await websession.delete(
+                    url=request_url + f"/{webhook_id}", data=params
+                )
+                if delete_response.status == 204:
+                    _LOGGER.info("Successfully Deleted Old Subscription")
+                    post_response = await websession.post(url=request_url, data=payload)
+                    _LOGGER.debug(f"second post response:{post_response.text()}")
+                else:
+                    raise Exception("Webhook Subscription could not be deleted")
+            else:
+                _LOGGER.info(
+                    f"Strava Webhook subscription for {CALLBACK_URL} already existed"
+                )
+        else:
+            raise Exception(
+                f"Webhook subscription returned an unexpected response: {post_response_content}"
+            )
+
+            # if the callback URL has changed, create a new subscription
+    """
+    OAuth2FlowHandler.async_register_implementation(
         hass,
         config_entry_oauth2_flow.LocalOAuth2Implementation(
             hass,
@@ -55,19 +112,43 @@ async def async_setup(hass: HomeAssistant, config: dict):
         ),
     )
 
+    hass.data[DOMAIN] = config
+    """
+    hass.async_create_task(
+        hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": SOURCE_IMPORT}, data=config
+        )
+    )
+    """
     return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up Strava Home Assistant from a config entry."""
-    implementation = await config_entry_oauth2_flow.async_get_config_entry_implementation(
-        hass, entry
-    )
+    """
+    Set up Strava Home Assistant from a config entry.
+    This function is called every time the system reboots
+    """
+    _LOGGER.debug("called async setup entry")
+    config = hass.data[DOMAIN]
+    try:
+        implementation = await config_entry_oauth2_flow.async_get_config_entry_implementation(
+            hass, entry
+        )
+    except KeyError:
+        _LOGGER.warning("HA Strava Component hasn't been set up in the UI")
+        return True
+
+    _LOGGER.debug("ha_strava found a valid OAuth implementation")
 
     session = config_entry_oauth2_flow.OAuth2Session(hass, entry, implementation)
 
+    _LOGGER.debug("ha_strava has created a running OAuth session")
+
     await session.async_ensure_token_valid()
-    print(session.token)
+
+    _LOGGER.debug("ha_strava has ensured that the token is valid")
+
+    _LOGGER.debug(f"ha_strava token:{session.token}")
 
     for component in PLATFORMS:
         hass.async_create_task(
@@ -88,7 +169,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
         )
     )
 
-    #if unload_ok:
+    # if unload_ok:
     #    hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
@@ -99,3 +180,43 @@ class StravaOAuth2Imlementation(config_entry_oauth2_flow.LocalOAuth2Implementati
     def redirect_uri(self) -> str:
         """Return the redirect uri."""
         return f"{get_url(self.hass, allow_internal=False, allow_ip=False)}{AUTH_CALLBACK_PATH}"
+
+
+class StravaSubscribeView(HomeAssistantView):
+    url = "/api/strava/webhook"
+    name = "api:strava:webhook"
+    requires_auth = False
+    cors_allowed = True
+
+    def __init__(self):
+        """Init the view."""
+        pass
+
+    async def get(self, request):
+        print("handling get request")
+        """Handle the incoming webhook challenge"""
+        webhook_subscription_challenge = request.query.get("hub.challenge", None)
+        print(f"webhook subscription challenge: {(webhook_subscription_challenge)}")
+        if webhook_subscription_challenge:
+            return json_response(
+                status=HTTP_OK, data={"hub.challenge": webhook_subscription_challenge}
+            )
+
+        return Response(status=HTTP_OK)
+
+
+class StravaWebhookView(HomeAssistantView):
+    url = "/api/strava/webhook"
+    name = "api:strava:webhook"
+    requires_auth = False
+    cors_allowed = True
+
+    def __init__(self):
+        """Init the view."""
+        pass
+
+    async def post(self, request):
+        data = await request.json()
+        print(type(data))
+        print(data)
+        return Response(status=HTTP_OK)
--- config/custom_components/ha_strava/config_flow.py
+++ config/custom_components/ha_strava/config_flow.py
@@ -1,9 +1,13 @@
 """Config flow for Strava Home Assistant."""
 import logging
-
+import asyncio
+import aiohttp
+import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.helpers import config_entry_oauth2_flow
-
+from homeassistant.helpers.network import get_url
+from homeassistant.const import CONF_CLIENT_ID, CONF_CLIENT_SECRET, HTTP_OK
+import aiohttp
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
@@ -33,4 +37,40 @@ class OAuth2FlowHandler(
 
     async def async_oauth_create_entry(self, data: dict) -> dict:
         print(f"oauth data: {data}")
+        async with aiohttp.ClientSession() as session:
+            payload = {
+                "client_id": "46186",
+                "client_secret": "6b3d8c8ab29664583925d2c93b1c12c2e0a21c78",
+                "callback_url": "http://9548b6f228ed.ngrok.io/api/strava/webhook",
+                "verify_token": "HA_STRAVA",
+            }
+
+            request_url = "https://www.strava.com/api/v3/push_subscriptions"
+
+            async with session.post(url=request_url, data=payload) as resp:
+                print(resp.status)
+                print(await resp.text())
+
         return self.async_create_entry(title="Strava Home Assistant", data=data)
+
+    async def async_step_import(self, config):
+        _LOGGER.debug("async step init")
+        _LOGGER.debug(f"config: {config}")
+        await asyncio.sleep(10)
+        request_url = "https://www.strava.com/api/v3/push_subscriptions"
+        CALLBACK_URL = f"{config['hass_url']}/api/strava/webhook"
+        payload = {
+            "client_id": config[DOMAIN][CONF_CLIENT_ID],
+            "client_secret": config[DOMAIN][CONF_CLIENT_SECRET],
+            "callback_url": CALLBACK_URL,
+            "verify_token": "HA_STRAVA",
+        }
+
+        _LOGGER.debug(f"callback url:{CALLBACK_URL}")
+        async with aiohttp.ClientSession() as websession:
+            post_response = await websession.post(url=request_url, data=payload)
+        _LOGGER.debug("did we get here?")
+        _LOGGER.debug(f"initial post response:{await post_response.text()}")
+        _LOGGER.debug(f"callback_url: {CALLBACK_URL}")
+
+        return self.async_create_entry(title="Strava Webhook", data={})
